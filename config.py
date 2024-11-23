7f7fe4f1d11408bc6018046122a0c2886c493d5a
debjyoti ghosh <debjyoti1ghosh@gmail.com>
2024-11-24 02:58:59 +0530

    location added

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config:
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'your-email-password')
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    ALLOWED_USER = os.getenv('ALLOWED_USERS')
+    ALLOWED_USER = os.getenv('ALLOWED_USER')
     PREDEFINED_PASSWORD = os.getenv('PREDEFINED_PASSWORD')
